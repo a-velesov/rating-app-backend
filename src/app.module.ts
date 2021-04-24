03e33cc04034ee0a667041be5b9e44b524cab457
Alexsandr <alexnem@inbox.ru>
2021-04-24 15:48:47 +0300

    add auth controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
@@ -8,8 +6,6 @@ import { ReviewModule } from './review/review.module';
 
 @Module({
 	imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
-	controllers: [AppController],
-	providers: [AppService]
 })
 export class AppModule {
 }
