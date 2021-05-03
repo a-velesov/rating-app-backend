8120be393801360780a6b7cfcbfa67d6eb45b142
Alexsandr <alexnem@inbox.ru>
2021-05-03 16:04:45 +0300

    mongo aggregation; added product service

---
+++ src/product/product.constans.ts
@@ -0,0 +1,1 @@
+export const PRODUCT_NOT_FOUND_ERROR = 'Продукт с таким id не найден';
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,32 +1,50 @@
-import { Body, Controller, Delete, Get, HttpCode, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, NotFoundException, Param, Post, Put, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ProductModel } from './product.model';
 import { FindProductDto } from './dto/find-product.dto';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ProductService } from './product.service';
+import { PRODUCT_NOT_FOUND_ERROR } from './product.constans';
 
 @Controller('product')
 export class ProductController {
 
-	@Post('create')
-	async create(@Body() dto: Omit<ProductModel, '_id'>) {
+	constructor(private readonly productService: ProductService) {}
 
+	@Post('create')
+	async create(@Body() dto: CreateProductDto) {
+		return this.productService.create(dto);
 	}
 
 	@Get(':id')
 	async get(@Param('id') id: string) {
-
+		const product = await this.productService.findById(id);
+		if (!product) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return product;
 	}
 
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
-
+		const deletedProduct = await this.productService.deleteById(id);
+		if (!deletedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
 	}
 
 	@Put(':id')
 	async put(@Param('id') id: string, @Body() dto:ProductModel) {
-
+		const updatedProduct = await this.productService.updateById(id, dto);
+		if (!updatedProduct) {
+			throw new NotFoundException(PRODUCT_NOT_FOUND_ERROR);
+		}
+		return updatedProduct;
 	}
-	@HttpCode(200)
-	@Post()
-	async find(@Body() dto: FindProductDto) {
 
+	@UsePipes(new ValidationPipe())
+	@HttpCode(200)
+	@Post('find')
+	async find(@Body() dto: FindProductDto) {
+		return this.productService.findWithReviews(dto);
 	}
 }
